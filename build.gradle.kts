437a735a21ce4cadd1d6dc9c7591e1fb6d3581d4
Florian Schmidt <florian.schmidt@platis.de>
2020-05-13 13:12:43 +0200

    Added modules ~template,~morphism,~state

---
+++ build.gradle.kts
@@ -0,0 +1,86 @@
+//group = "org.evoleq"
+//version = "1.0.0"
+
+
+plugins {
+    //java
+    kotlin("multiplatform") version "1.3.70"
+    id ("com.github.hierynomus.license") version "0.15.0"
+    `maven-publish`
+    maven
+    id ("com.jfrog.bintray") version "1.8.0"
+    id("org.jetbrains.dokka") version "0.9.17"
+}
+
+group = Config.Module.Template.group
+version = Config.Module.Template.version//+"-SNAPSHOT"
+
+repositories {
+    mavenLocal()
+    mavenCentral()
+}
+/*
+kotlin {
+    /* Targets configuration omitted.
+    *  To find out how to configure the targets, please follow the link:
+    *  https://kotlinlang.org/docs/reference/building-mpp-with-gradle.html#setting-up-targets */
+    jvm().compilations["main"].defaultSourceSet {
+        dependencies {
+            implementation(kotlin("stdlib-jdk8"))
+            implementation(kotlin("reflect"))
+            implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.5")
+        }
+    }
+    // JVM-specific tests and their dependencies:
+    jvm().compilations["test"].defaultSourceSet {
+        dependencies {
+            implementation(kotlin("test-junit"))
+        }
+    }
+    
+    js().compilations["main"].defaultSourceSet  {
+        dependencies {
+            //implementation(kotlin("js"))
+            implementation(kotlin("reflect"))
+            implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core-js:1.3.5")
+        }/*  */
+    }
+    js().compilations["test"].defaultSourceSet {/* ... */ }
+    
+    sourceSets {
+        val commonMain by getting {
+            dependencies {
+                implementation(kotlin("stdlib-common"))
+                implementation(kotlin("reflect"))
+                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core-common:1.3.5")
+                implementation(project(":mathcat-core"))
+            }
+        }
+        val commonTest by getting {
+            dependencies {
+                implementation(kotlin("test-common"))
+                implementation(kotlin("test-annotations-common"))
+            }
+        }
+    }
+}
+*/
+/*
+tasks{
+    val licenseFormatJsMain by creating(com.hierynomus.gradle.license.tasks.LicenseFormat::class) {
+        source = fileTree("$projectDir/src/jsMain/kotlin") {
+        }
+        group = "license"
+    }
+    val licenseFormatCommonMain by creating(com.hierynomus.gradle.license.tasks.LicenseFormat::class) {
+        source = fileTree("$projectDir/src/commonMain/kotlin") {
+        }
+        group = "license"
+    }
+    licenseFormat {
+        finalizedBy(licenseFormatJsMain, licenseFormatCommonMain)
+    }
+}
+
+ */
+
